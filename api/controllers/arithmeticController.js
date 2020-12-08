105f135574556b1a11d39c1d321d09f43bab3193
VictorChin <victor@objectMage.com>
2020-12-08 02:19:49 -0800

    Add comment to explain addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //using the + operator to treate a and b as numbers
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
